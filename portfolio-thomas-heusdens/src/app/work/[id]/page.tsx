80ca0e82c32747efc974f287d41e4617cf990521
ThomasHeusdens <pilouthomas03@gmail.com>
2024-12-07 23:18:13 +0100

    Changed the link of the fetch from local to vercel

--- portfolio-thomas-heusdens/src/app/work/[id]/page.tsx
+++ portfolio-thomas-heusdens/src/app/work/[id]/page.tsx
@@ -3,7 +3,7 @@ import ProjectDetails from "./ProjectDetails";
 import { Project } from "@/types";
 
 async function fetchProjects(): Promise<Project[]> {
-  const response = await fetch("http://localhost:3000//projects.json");
+  const response = await fetch("https://portfolio-p7gcxv2lz-thomasheusdens-projects.vercel.app/projects.json");
   if (!response.ok) {
     throw new Error("Failed to fetch projects");
   }
@@ -13,9 +13,9 @@ async function fetchProjects(): Promise<Project[]> {
 }
 
 export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params; // Await the params object
-  const projects = await fetchProjects(); // Fetch the projects
-  const project = projects.find((proj) => proj.id === id); // Find the project by ID
+  const { id } = await params;
+  const projects = await fetchProjects();
+  const project = projects.find((proj) => proj.id === id);
 
   if (!project) {
     return notFound();
